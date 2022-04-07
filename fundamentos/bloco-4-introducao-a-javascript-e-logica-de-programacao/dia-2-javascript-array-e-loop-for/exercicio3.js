a4a39f6a95ad48899d9655ecede6982e17642c59
Pedro Papini <ppapinia@gmail.com>
2022-04-07 18:39:09 -0300

    Adiciona exercício 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio3.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let media;
+
+for (let num of numbers) {
+    sum += num;
+}
+
+media = sum / numbers.length;
+
+console.log(media);
