c2e3fa007bdcd7fb85073b3090009c8a1bf9ccf6
Pedro Papini <ppapinia@gmail.com>
2022-05-06 16:01:23 -0300

    Resolve exercício 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
@@ -1,4 +1,5 @@
-function testingScope(escopo) {
+
+/* function testingScope(escopo) {
   if (escopo === true) {
     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
@@ -8,6 +9,17 @@ function testingScope(escopo) {
     console.log(elseScope);
   }
   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-}
+} */
 
-testingScope(true);
+const testingScope = escopo => {
+  if (escopo) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  } 
+};
+
+testingScope(false);
