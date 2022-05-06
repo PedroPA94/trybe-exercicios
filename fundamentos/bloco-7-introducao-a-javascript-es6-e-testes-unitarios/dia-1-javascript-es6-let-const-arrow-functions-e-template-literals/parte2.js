f004e3074fbe2eba352332b304c354de158aef1d
Pedro Papini <ppapinia@gmail.com>
2022-05-06 18:59:00 -0300

    Resolve exercício 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
@@ -13,6 +13,22 @@ console.log(findFactorial(4));
 
 // Exercício 1 - Bônus
 
-const findFactorialWithRecursion = (positiveInteger) => positiveInteger === 1 ? positiveInteger : positiveInteger * findFactorialWithRecursion(positiveInteger -1);
+const findFactorialWithRecursion = (positiveInteger) => positiveInteger === 1 ? positiveInteger : positiveInteger * findFactorialWithRecursion(positiveInteger - 1);
 
 console.log(findFactorialWithRecursion(4));
+
+
+// Exercício 2
+
+const longestWord = (sentence) => {
+  const sentenceArray = sentence.split(' ');
+  let longest = sentenceArray[0]
+  for (let word of sentenceArray) {
+    if (word.length > longest.length) {
+      longest = word;
+    }
+  }
+  return longest;
+}
+
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu')) // retorna 'aconteceu'
