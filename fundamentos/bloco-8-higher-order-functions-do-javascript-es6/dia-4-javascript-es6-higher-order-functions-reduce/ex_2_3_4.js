b2083cb244367d55685008687530a3a0412d7321
Pedro Papini <ppapinia@gmail.com>
2022-05-18 15:25:05 -0300

    Resolve exercícios 2, 3 e 4

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/ex_2_3_4.js
@@ -0,0 +1,92 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+
+// Exercício 2
+
+
+function reduceNames(arr) {
+  return arr.reduce((acc, current, index) => 
+  index < arr.length - 1 ? acc.concat(`${current.author.name}, `) : acc.concat(`${current.author.name}.`)
+  , '');
+}
+
+console.log(reduceNames(books));
+
+
+// Exercício 3
+
+function averageAge(arr) {
+  return arr.reduce((acc, current) => acc + current.releaseYear - current.author.birthYear, 0) / arr.length;
+}
+
+console.log(averageAge(books));
+
+
+// Exercício 4
+
+function longestNamedBook(arr) {
+  return arr.reduce((acc, current) => acc.name.length > current.name.length ? acc : current);
+}
+
+console.log(longestNamedBook(books));
