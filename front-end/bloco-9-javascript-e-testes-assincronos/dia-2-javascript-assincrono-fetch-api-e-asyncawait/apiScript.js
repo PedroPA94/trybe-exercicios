5d6597fba052517a1b78f0b4c113145ed4b7d646
Pedro Papini <ppapinia@gmail.com>
2022-06-02 11:11:14 -0300

    Resolve exercício 1

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
@@ -1,8 +1,21 @@
 // apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
+const addJoke = (joke) => {
+  const jokeContainer = document.getElementById('jokeContainer');
+  jokeContainer.textContent = joke;
+}
+
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => addJoke(data.joke))
+    .catch(err => console.log(err));
 };
 
 window.onload = () => fetchJoke();
