d18f802476bdfaf348fdb352cc4d9bc53284ee8e
Pedro Papini <ppapinia@gmail.com>
2022-09-28 19:35:46 -0300

    Resolve exercícios

--- back-end/secao4/dia2/src/server.js
+++ back-end/secao4/dia2/src/server.js
@@ -1,3 +1,26 @@
 const app = require('./app');
+const activities = require('./activities');
 
-app.listen(3001, () => console.log('server running on port 3001'));
+const port = 3001;
+
+app.listen(port, () => console.log(`Server running on port ${port}.`));
+
+app.get('/myActivities', (_req, res) => {
+  res.status(200).json(activities);
+});
+
+app.get('/myActivities/:id', (req, res) => {
+  const { id } = req.params;
+  const selectedActivity = activities.find((act) => act.id === Number(id));
+
+  if (selectedActivity) res.status(200).json(selectedActivity);
+  res.status(400).send('Bad request: invalid id');
+});
+
+app.get('/filter/myActivities', (req, res) => {
+  const { status } = req.query;
+  const filteredActivities = activities
+    .filter((act) => act.status.toLowerCase() === status.toLowerCase());
+
+  res.status(200).json(filteredActivities);
+});
