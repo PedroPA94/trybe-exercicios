7b8f5a973a6b1fd252e7b7babf66f5c8d7ec0042
Pedro Papini <ppapinia@gmail.com>
2023-03-08 18:59:53 -0300

    Resolve exercício 5

---
+++ cs/secao3/dia1/ex5.py
@@ -0,0 +1,21 @@
+import requests
+from parsel import Selector
+
+root_url = 'http://books.toscrape.com/'
+book_url = root_url + 'catalogue/the-grand-design_405/index.html'
+
+response = requests.get(book_url)
+selector = Selector(response.text)
+
+title = selector.css('.product_main h1::text').get()
+price = selector.css('.price_color::text').get()[2:]
+description = selector.css('#product_description ~ p::text').get()
+cover_url = selector.css('.thumbnail img::attr(src)').get()
+
+in_stock = selector.css('.product_main .availability::text').re_first(r'\d+')
+
+suffix = '...more'
+if description and description.endswith(suffix):
+    description = description[:-len(suffix)]
+
+print(f'{title},{price},{description},{root_url + cover_url},{in_stock}')
