d3bf08979a9dcc15730ae6481bcb5e07af38f35c
Pedro Papini <ppapinia@gmail.com>
2023-03-09 13:53:44 -0300

    Resolve exercício 7

---
+++ cs/secao3/dia1/ex7.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+
+
+with MongoClient() as client:
+    db = client.library
+    books = db.books
+
+    pipeline = [
+        {
+            '$unwind': '$categories',
+        },
+        {
+            '$group': {
+                '_id': '$categories',
+                'count': {'$sum': 1}
+            }
+        },
+        {
+            '$sort': {
+                'count': -1
+            }
+        }
+    ]
+
+    categories_count = books.aggregate(pipeline)
+
+    for category in categories_count:
+        print(category['_id'], category['count'])
